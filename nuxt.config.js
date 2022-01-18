461775bd4c3a8ad8ee0a6862894023ed599f4865
JZimz <66445902+JZimz@users.noreply.github.com>
2022-01-18 16:47:22 -0500

    feat: migrate site to nuxt (#1)
    This change migrates the site to Nuxt configured statically along with a slight redesign to the home page. It also includes addition to some developer tools such as lint and commit style enforcement.
    
    * feat: migrate site to nuxt
    
    * feat: update hero styling
    
    * style: Add lintfix to husky

---
+++ nuxt.config.js
@@ -0,0 +1,122 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'jzimz.com',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/favicon/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon/favicon-16x16.png',
+      },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+    // https://google-fonts.nuxtjs.org/
+    '@nuxtjs/google-fonts',
+    // https://google-analytics.nuxtjs.org/
+    '@nuxtjs/google-analytics',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+  ],
+
+  router: {
+    extendRoutes(routes, resolve) {
+      const component = resolve(__dirname, 'components/redirect-link.vue')
+      const redirects = [
+        {
+          name: 'twitch',
+          to: 'https://www.twitch.tv/jzimz',
+          toDeep: 'twitch://stream/jzimz',
+        },
+        { name: 'twitter', to: 'https://twitter.com/jzimz' },
+        { name: 'youtube', to: 'https://www.youtube.com/jzimz' },
+        { name: 'yt', to: 'https://www.youtube.com/jzimz' },
+        { name: 'discord', to: 'https://discord.gg/aV37twc' },
+        {
+          name: 'downloads',
+          to: 'https://drive.google.com/drive/folders/1VgERaPXuk9pz-YFNMO9emE0FnOT3kRVk?usp=sharing',
+        },
+        { name: 'bio-template', to: 'https://github.com/jzimz/bio-template' },
+      ]
+
+      // Add convenience redirects.
+      redirects.forEach(({ name, to, toDeep }) => {
+        routes.push({
+          name,
+          path: `/${name}`,
+          component,
+          props: {
+            msg: `Sending you over to ${name}.`,
+            to,
+            toDeep,
+          },
+        })
+      })
+    },
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+
+  // Google Analytics configuration: https://google-analytics.nuxtjs.org/options
+  googleAnalytics: {
+    id: 'UA-170360802-1',
+  },
+
+  // Google Fonts configuration: https://google-fonts.nuxtjs.org/options
+  googleFonts: {
+    families: {
+      Poppins: {
+        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        ital: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      },
+    },
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="page-home">
+    <section class="hero">
+      <div class="hero-photo"></div>
+      <div class="hero-content">
+        <strong>Hello!</strong> My name is Jason but most know me as JZimz
+        <em>(jay zimz)</em> online. I'm a full-time web developer that loves
+        gaming and the process of creating content. Lately, you'll find me
+        playing Warzone but I'm into most FPS games and puzzle platformers. If
+        you'd like to get in touch,
+        <a
+          target="_blank"
+          href="https://twitter.com/messages/compose?recipient_id=1226548642397310976"
+          class="twitter-dm-link"
+          >my DMs are open!</a
+        >
+      </div>
+    </section>
+    <section class="linkage">
+      <a href="https://www.twitch.tv/jzimz" target="_blank" class="link-item">
+        <twitch-icon size="3x" />
+      </a>
+      <a
+        href="https://www.youtube.com/channel/UCxyaFKlRSXJ-D0-UICa6W3g/"
+        target="_blank"
+        class="link-item"
+      >
+        <youtube-icon size="3x" />
+      </a>
+      <a
+        href="https://www.instagram.com/jzimztv/"
+        target="_blank"
+        class="link-item"
+      >
+        <instagram-icon size="3x" />
+      </a>
+      <a href="https://twitter.com/jzimz" target="_blank" class="link-item">
+        <twitter-icon size="3x" />
+      </a>
+      <a href="http://buymeacoffee.com/jzimz" target="_blank" class="link-item">
+        <coffee-icon size="3x" />
+      </a>
+    </section>
+    <!-- TODO: WIP <section-divider />
+    <section class="articles">
+      <h2>Latest Articles</h2>
+      <article-list :articles="articles" />
+    </section> -->
+  </div>
+</template>
+
+<script>
+import {
+  TwitchIcon,
+  TwitterIcon,
+  CoffeeIcon,
+  YoutubeIcon,
+  InstagramIcon,
+} from 'vue-feather-icons'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    TwitchIcon,
+    TwitterIcon,
+    CoffeeIcon,
+    YoutubeIcon,
+    InstagramIcon,
+  },
+  // TODO: Articles are a WIP
+  // async asyncData ({ $content, params }) {
+  //   const articles = await $content('articles')
+  //     .only(['title', 'slug', 'createdAt', 'description'])
+  //     .sortBy('age', 'desc')
+  //     .limit(3)
+  //     .fetch()
+
+  //   return {
+  //     articles
+  //   }
+  // },
+}
+</script>
+
+<style lang="less">
+.page-home {
+  // TODO: Make a base site style
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+  @black: #0d0f11;
+  @white: #ffffff;
+  strong {
+    font-weight: 600;
+  }
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  section {
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+    max-width: 520px;
+
+    &:not(:last-child) {
+      margin-bottom: 30px;
+    }
+  }
+
+  // TODO: It's probably bad to have these all be the same color so I need to
+  // look into this more.
+  a:link {
+    color: @black;
+  }
+  a:visited {
+    color: @black;
+  }
+  a:hover {
+    color: @black;
+  }
+  a:active {
+    color: @black;
+  }
+
+  .hero {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 30px;
+
+    &-photo {
+      width: 300px;
+      height: 300px;
+      background-color: @white;
+      border-radius: 50%;
+      background-repeat: no-repeat;
+      background-image: url('/img/profile.png');
+      background-size: 100%;
+      border: solid 4px @black;
+      filter: drop-shadow(0.35rem 0.35rem 0.4rem rgba(0, 0, 0, 0.5));
+
+      &:hover {
+        background-image: url('/img/profile-alt.png');
+        background-size: 100%;
+      }
+    }
+
+    &-content {
+      margin-top: 20px;
+    }
+  }
+
+  .linkage {
+    text-align: center;
+    display: flex;
+    justify-content: space-evenly;
+
+    .feather {
+      stroke: @black;
+    }
+  }
+}
+</style>
