294bcdf76540d5e3473bd7101e8e4f82b671e368
JZimz <JZimz@users.noreply.github.com>
2022-01-18 22:16:58 +0000

    deploy: a53cf4d5dac697dce041fb1f0ca96f6e46fa03fa

--- _nuxt/static/1642204391/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Ftwitch","\u002Fyt","\u002Fdownloads","\u002Ftwitter","\u002Fyoutube","\u002Fbio-template","\u002Fdiscord"]})
---
+++ _nuxt/static/1642544217/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Ftwitch","\u002F","\u002Ftwitter","\u002Fyoutube","\u002Fyt","\u002Fdiscord","\u002Fdownloads","\u002Fbio-template"]})
