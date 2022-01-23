2328e13c1e88a1dd0cb9464f683184a7b3520c36
JZimz <jzimz.tv@gmail.com>
2022-01-22 23:50:15 -0500

    feat: add brand icons

--- pages/index.vue
+++ pages/index.vue
@@ -18,27 +18,30 @@
     </section>
     <section class="linkage">
       <a href="https://www.twitch.tv/jzimz" target="_blank" class="link-item">
-        <twitch-icon size="3x" />
+        <twitch-icon size="2x" />
       </a>
       <a
         href="https://www.youtube.com/channel/UCxyaFKlRSXJ-D0-UICa6W3g/"
         target="_blank"
         class="link-item"
       >
-        <youtube-icon size="3x" />
+        <you-tube-icon size="2x" />
+      </a>
+      <a href="https://www.tiktok.com/@jzimz" target="_blank" class="link-item">
+        <tik-tok-icon size="2x" />
       </a>
       <a
         href="https://www.instagram.com/jzimztv/"
         target="_blank"
         class="link-item"
       >
-        <instagram-icon size="3x" />
+        <instagram-icon size="2x" />
       </a>
       <a href="https://twitter.com/jzimz" target="_blank" class="link-item">
-        <twitter-icon size="3x" />
+        <twitter-icon size="2x" />
       </a>
       <a href="http://buymeacoffee.com/jzimz" target="_blank" class="link-item">
-        <coffee-icon size="3x" />
+        <buy-me-a-coffee-icon size="2x" />
       </a>
     </section>
     <!-- TODO: WIP <section-divider />
@@ -51,21 +54,23 @@
 
 <script>
 import {
+  YouTubeIcon,
   TwitchIcon,
-  TwitterIcon,
-  CoffeeIcon,
-  YoutubeIcon,
+  TikTokIcon,
   InstagramIcon,
-} from 'vue-feather-icons'
+  TwitterIcon,
+  BuyMeACoffeeIcon,
+} from 'vue-simple-icons'
 
 export default {
   name: 'IndexPage',
   components: {
+    YouTubeIcon,
     TwitchIcon,
-    TwitterIcon,
-    CoffeeIcon,
-    YoutubeIcon,
+    TikTokIcon,
     InstagramIcon,
+    TwitterIcon,
+    BuyMeACoffeeIcon,
   },
   head: {
     // TODO: See if there is already a library that will translate a JSON object
@@ -167,6 +172,9 @@ export default {
 
     .feather {
       stroke: var(--color);
+    }
+    svg {
+      fill: var(--color);
     }
   }
 }
