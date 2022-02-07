9992f48e11ebebaa03c466243ebbcc15d9a699b9
JZimz <JZimz@users.noreply.github.com>
2022-02-07 17:08:38 +0000

    deploy: cc7328a1294861087af6a8a95d0fc51d9c780cd1

--- _nuxt/static/1642913601/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Ftwitch","\u002F","\u002Ftwitter","\u002Fyoutube","\u002Fyt","\u002Fdiscord","\u002Fdownloads","\u002Fbio-template"]})
---
+++ _nuxt/static/1644253717/downloads/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644253717",layout:"default",error:a,serverRendered:b,routePath:"\u002Fdownloads",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7f1c5ad3"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
