8998026ea3c95949aab88fb97ddab4d80ea354a8
JZimz <JZimz@users.noreply.github.com>
2022-02-18 03:38:12 +0000

    deploy: f83210477a33a9e7e32e6a504c46a73fdfd73173

--- _nuxt/static/1644611389/downloads/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644611389",layout:"default",error:a,serverRendered:b,routePath:"\u002Fdownloads",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0e881948"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
