5855636bbb47898f2beb6ff2136b68b849b515e5
JZimz <66445902+JZimz@users.noreply.github.com>
2023-07-11 10:33:05 -0400

    chore: update twitch vod to latest

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="page-home">
     <section class="hero">
       <!-- <div class="hero-photo"></div> -->
-      <twitch-embed video="1829350994" />
+      <twitch-embed video="1866096776" />
       <div class="linkage">
         <a
           href="https://www.twitch.tv/jzimz"
